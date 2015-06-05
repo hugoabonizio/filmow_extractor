74bd198e8c9c6bac7c2bbe3f787c13950d125345
hugoabonizio <hugo_abonizio@hotmail.com>
2015-06-05 19:09:49 +0000

    Better poster downloading

--- download_posters.rb
+++ download_posters.rb
@@ -8,19 +8,31 @@ ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 class Serie < ActiveRecord::Base
 end
 
-pool = Thread.pool(40)
-series = Serie.all
-puts "Total: #{series.size}"
-
-series.each do |s|
-	puts "Downloading #{s.title}"
-	pool.process {
-		File.open("poster/#{rand(16 ** 32)}.jpg", 'wb') do |f|
-			f.write open(s.poster,
-				"Referer" => "http://thetvdb.com/?tab=series&id=79349",
-				"User-Agent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
-			).read 
+def download(series)
+	pool = Thread.pool(10)
+	error = []
+	series.each do |s|
+		pool.process do
+			unless File.exists? "poster/#{s.title}.jpg"
+				puts "Downloading: #{s.title}"
+				result = open(s.poster,	"Referer" => "http://thetvdb.com/?tab=series&id=#{rand(1_000_000)}", "User-Agent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36").read
+				if result.size > 0
+					File.open("poster/#{s.title}.jpg", 'wb') do |f|
+						f.write result
+					end
+				else
+					error << s
+				end
+			end
 		end
-	}
+	end
+	pool.shutdown
+	puts "Erro em: #{error.size}"
+	sleep 5
+	download(error)
 end
-pool.shutdown
+
+all = Serie.all
+puts "Total: #{all.size}"
+
+download(all)
